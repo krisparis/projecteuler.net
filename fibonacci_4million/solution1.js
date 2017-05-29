56a460d8a25a266a0c952b728b018031f9a28a3c
krisparis <christophe.elavumoottil@gmail.com>
2017-05-29 12:57:04 +0200

    Solution 1
    solution 1

---
+++ fibonacci_4million/solution1.js
@@ -0,0 +1,18 @@
+var evenSumfibonacciNbsBelow = function(limit) {
+    var a = 0, b = 1, c = a + b,
+    // Initialize 'evensum' with the first fibonacci number which is even.
+    evensum = 0;
+    
+    // While the third calculated fibonacci number is smaller than limit.
+    while (c < limit) {
+            // Add only if 'a' is even.
+        if (c % 2 === 0) {
+            evensum += c;
+        }    
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return evensum;
+}
+console.log(evenSumfibonacciNbsBelow(4000000)); // 4613732
