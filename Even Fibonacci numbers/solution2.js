576b626edb63dbad1604143eddc6d2aa44859ad9
krisparis <christophe.elavumoottil@gmail.com>
2017-05-30 22:24:33 +0200

    Update the proof within the comments

--- Even Fibonacci numbers/solution2.js
+++ Even Fibonacci numbers/solution2.js
@@ -1,16 +1,3 @@
-// If n multiple of 3 then is F(n) even?
-
-// Proof by induction:
-// We admit F(3p) is even. p belongs to Z.
-// Is F(3(p+1)) F(3p+3) even for ever p in Z ?
-
-// Proof:
-// F(3p+3) = F(3p+2) + F(3p+1)
-// F(3p+3) = F(3p+1) + F(3p) + F(3p+1)
-// F(3p+3) = F(3p) + 2*F(3p+1)
-// ==> F(3p+3) is even
-
-
 var sumOfEvenFibonacciNbsBelowLimit = function(limit) {
     var a = 1, b = 1, c = a+b, evenTermsSum = 0;
     
@@ -25,3 +12,18 @@ var sumOfEvenFibonacciNbsBelowLimit = function(limit) {
 }
 
  console.log(sumOfEvenFibonacciNbsBelowLimit(4000000)); // 4613732
+ 
+// Proove that only every third fibonacci number is even.
+//
+// By induction.
+// Initial case:
+// F(0) = 0, F(1) = 1, F(2) = 1, F(3) = 2. ==> Only F(0) and F(3) are even whereas F(1) and F(2) are odd.
+// We admit that F(n) is even and also F(n-1) and F(n-2) are odd.
+// We have to proove that F(n+3) is even and F(n+1), F(n+2) are odd.
+
+// F(n) = F(n-1) + F(n-2)
+// F(n+1) = F(n) + F(n-1) => Sum between even and odd number is odd. So F(n+1) is odd.
+// F(n+2) = F(n+1) + F(n) => Sum between even and odd number is odd. So F(n+2) is odd.
+// F(n+3) = F(n+2) + F(n+1) => Sum between two odd number is even. So F(n+3) is even.
+//
+// End of proof.
