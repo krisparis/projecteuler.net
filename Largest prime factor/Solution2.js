64ec7cccd9c4556ce511eb1e6f92039896e9520c
krisparis <christophe.elavumoottil@gmail.com>
2017-06-13 17:03:33 +0200

    (style): delete any tab space characters

--- Largest prime factor/Solution2.js
+++ Largest prime factor/Solution2.js
@@ -1,16 +1,17 @@
 // Algorithm that uses the property that
 // each number is a product of prime number n = p1^a1 * p2^a2 * ... * pn^an.
 
+    
 var n = 600851475143,
 k = 2, lastFactor = 1;
 while (n > 1){
     // If k divide n.
     if (n % k === 0)  {
-	    // lastFactor will always be a prime number
-		// because the first k is 2, a prime number, and because of the following code.
+        // lastFactor will always be a prime number
+        // because the first k is 2, a prime number, and because of the following code.
         lastFactor = k;
         n = Math.floor(n/k);
-		// Continue division while n is a multiple of k.
+        // Continue division while n is a multiple of k.
         while (n % k === 0){
             n = Math.floor(n/k);
         }
